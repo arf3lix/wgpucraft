0066f0fcc5d00afc627c08c9d2cabbe621c8066b
arf3lix <felix.sanchez.dev@gmail.com>
2025-05-22 21:01:52 -0500

    is better to have a create render pipeline function

--- src/render/pipelines/terrain.rs
+++ src/render/pipelines/terrain.rs
@@ -32,79 +32,72 @@ impl Vertex for BlockVertex {
     }
 }
 
-pub struct TerrainPipeline {
-    pub pipeline: RenderPipeline
-}
 
-impl TerrainPipeline {
-    pub fn new(
-        device: &wgpu::Device,
-        global_layout: &GlobalsLayouts,
-        shader: wgpu::ShaderModule,
-        config: &wgpu::SurfaceConfiguration,//in the future i better add a config struct global
-    ) -> Self {
+pub fn create_terrain_pipeline(
+    device: &wgpu::Device,
+    global_layout: &GlobalsLayouts,
+    shader: wgpu::ShaderModule,
+    config: &wgpu::SurfaceConfiguration,//in the future i better add a config struct global
+) -> RenderPipeline {
 
-        let pipeline_layout =
-        device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
-            label: Some("Render Figure Pipeline Layout"),
-            bind_group_layouts: &[
-                &global_layout.atlas_layout,
-                &global_layout.globals,
-                // pendiente agregar layout con algo relacionado del terreno
-            ],
-            push_constant_ranges: &[],
-        });
+    let pipeline_layout =
+    device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: Some("Render Figure Pipeline Layout"),
+        bind_group_layouts: &[
+            &global_layout.atlas_layout,
+            &global_layout.globals,
+            // pendiente agregar layout con algo relacionado del terreno
+        ],
+        push_constant_ranges: &[],
+    });
 
-        let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-            label: Some("Render generic Pipeline"),
-            layout: Some(&pipeline_layout),
-            primitive: wgpu::PrimitiveState { 
-                topology: wgpu::PrimitiveTopology::TriangleList,
-                strip_index_format: None,
-                front_face: wgpu::FrontFace::Ccw,
-                cull_mode: Some(wgpu::Face::Back),
-                // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                polygon_mode: wgpu::PolygonMode::Fill,
-                // Requires Features::DEPTH_CLIP_CONTROL
-                unclipped_depth: false,
-                // Requires Features::CONSERVATIVE_RASTERIZATION
-                conservative: false,
-            },
-            vertex: wgpu::VertexState {
-                module: &shader,
-                entry_point: "vs_main",
-                buffers: &[BlockVertex::desc()],
-                compilation_options: Default::default(),
-            },
-            fragment: Some(wgpu::FragmentState {
-                module: &shader,
-                entry_point: "fs_main",
-                targets: &[Some(wgpu::ColorTargetState {
-                    format: config.format,
-                    blend: Some(wgpu::BlendState::REPLACE),
-                    write_mask: wgpu::ColorWrites::ALL,
-                })],
-                compilation_options: Default::default(),
-            }),
-            depth_stencil: Some(wgpu::DepthStencilState {
-                format: Texture::DEPTH_FORMAT,
-                depth_write_enabled: true,
-                depth_compare: wgpu::CompareFunction::Less,
-                stencil: wgpu::StencilState::default(),
-                bias: wgpu::DepthBiasState::default(),
-            }),
-            multisample: wgpu::MultisampleState {
-                count: 1,
-                mask: !0,
-                alpha_to_coverage_enabled: false,
-            },
-            multiview: None,
-            cache: None,
-        });
+    let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("Render generic Pipeline"),
+        layout: Some(&pipeline_layout),
+        primitive: wgpu::PrimitiveState { 
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            strip_index_format: None,
+            front_face: wgpu::FrontFace::Ccw,
+            cull_mode: Some(wgpu::Face::Back),
+            // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+            polygon_mode: wgpu::PolygonMode::Fill,
+            // Requires Features::DEPTH_CLIP_CONTROL
+            unclipped_depth: false,
+            // Requires Features::CONSERVATIVE_RASTERIZATION
+            conservative: false,
+        },
+        vertex: wgpu::VertexState {
+            module: &shader,
+            entry_point: Some("vs_main"),
+            buffers: &[BlockVertex::desc()],
+            compilation_options: Default::default(),
+        },
+        fragment: Some(wgpu::FragmentState {
+            module: &shader,
+            entry_point: Some("fs_main"),
+            targets: &[Some(wgpu::ColorTargetState {
+                format: config.format,
+                blend: Some(wgpu::BlendState::REPLACE),
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+            compilation_options: Default::default(),
+        }),
+        depth_stencil: Some(wgpu::DepthStencilState {
+            format: Texture::DEPTH_FORMAT,
+            depth_write_enabled: true,
+            depth_compare: wgpu::CompareFunction::Less,
+            stencil: wgpu::StencilState::default(),
+            bias: wgpu::DepthBiasState::default(),
+        }),
+        multisample: wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        },
+        multiview: None,
+        cache: None,
+    });
 
-        Self {
-            pipeline
-        }
+    pipeline
 
-    }
 }
